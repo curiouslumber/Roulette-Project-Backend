4e9103793aa9cb1969412aedf276f89c81738b3d
Noel Pinto <noelpinto47@gmail.com>
2023-11-10 17:30:21 +0530

    updated

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mysql = require('mysql');
+const bodyParser = require('body-parser');
 
 const app = express();
+const PORT = 3000;
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -10,31 +12,84 @@ const connection = mysql.createConnection({
     database: 'roulette_game'
 });
 
-connection.connect(err => {
+// Body parser middleware to parse POST request bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes for CRUD operations
+
+// Create
+app.post('/users', (req, res) => {
+  const { email, role } = req.body;
+  const query = 'INSERT INTO Users (email, role) VALUES (?, ?)';
+
+  connection.query(query, [email, role], (err, result) => {
     if (err) {
-      console.error('Error connecting to MySQL:', err);
+      console.error('Error executing query:', err);
+      res.status(500).send('Internal Server Error');
       return;
     }
-    console.log('Connected to MySQL');
+
+    res.json({ id: result.insertId, email, role });
   });
-  
-  // Define your route to fetch data
-  app.get('/users', (req, res) => {
-    const query = 'SELECT * FROM Users';
-  
-    connection.query(query, (err, results) => {
-      if (err) {
-        console.error('Error executing query:', err);
-        res.status(500).send('Internal Server Error');
-        return;
-      }
-  
-      res.json(results);
-    });
+});
+
+// Read all
+app.get('/users', (req, res) => {
+  const query = 'SELECT * FROM Users';
+
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error('Error executing query:', err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    res.json(results);
   });
-  
-  // Start the server
-  const PORT = 3000;
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+// Read one
+app.get('/users/:id', (req, res) => {
+  const userId = req.params.id;
+  const query = 'SELECT * FROM Users WHERE user_id = ?';
+
+  connection.query(query, [userId], (err, results) => {
+    if (err) {
+      console.error('Error executing query:', err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    if (results.length === 0) {
+      res.status(404).send('User not found');
+    } else {
+      res.json(results[0]);
+    }
   });
+});
+
+// Delete
+app.delete('/users/:id', (req, res) => {
+  const userId = req.params.id;
+  const query = 'DELETE FROM Users WHERE user_id = ?';
+
+  connection.query(query, [userId], (err, result) => {
+    if (err) {
+      console.error('Error executing query:', err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    if (result.affectedRows === 0) {
+      res.status(404).send('User not found');
+    } else {
+      res.status(204).send(); // No content
+    }
+  });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
